c9d3c702d014af960f9db6fdf1a9e2923cb1e627
Nicholas1224 <nicholaschan082@gmail.com>
2023-03-25 14:23:55 -0700

    first commit

---
+++ git-history.sh
@@ -0,0 +1,40 @@
+for Y in {2023..2023}
+do
+  mkdir $Y
+  cd $Y
+  for M in {1..1}
+  do
+    mkdir $M
+    cd $M
+    for D in {10..31}
+    do
+      dayofdate=$(date -d "$Y-$M-$D" '+%a');
+
+      # if [ "Sun" == $dayofdate ]; then continue; fi
+      # if [ "Sat" == $dayofdate ]; then continue; fi
+      # if [ "Mon" == $dayofdate ]; then continue;
+      # if [ "Tue" == $dayofdate ]; then continue;
+      # if [ "Wed" == $dayofdate ]; then continue;
+      # if [ "Thu" == $dayofdate ]; then continue;
+      # if [ "Fri" == $dayofdate ]; then continue;
+
+      mkdir $D
+      cd $D
+      for i in $(seq 1 $(($RANDOM%5)))
+       do
+	      echo "0$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:0$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:0$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "0$i on $M $D $Y"
+       done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push
+git rm -rf 20**
+git commit -am "cleanup"
+git push
